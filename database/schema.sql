651cf5a2500c9c63ab2c4a3dd4e67422f28956c3
Md Shoaib Md Mukhtar Bagwan <shoaibshaikh643@gmail.com>
2025-10-09 14:12:05 +0530

    update schema 13

--- database/schema.sql
+++ database/schema.sql
@@ -1,14 +1,6 @@
-DELIMITER $$ 
-CREATE TRIGGER `generate_share_code` 
-BEFORE INSERT ON `shares` 
-FOR EACH ROW BEGIN 
-IF NEW.share_code IS NULL THEN
-        SET NEW.share_code = CONCAT('JK', UPPER(SUBSTRING(MD5(CONCAT(NEW.design_id, NEW.user_id, NOW())), 1, 8)));
-    END IF; 
-END$$ 
-DELIMITER; 
-SET FOREIGN_KEY_CHECKS = 1;
 
+SET FOREIGN_KEY_CHECKS = 1;
+ 
 SELECT 'Database schema created successfully!' as status;
 SELECT 
     TABLE_NAME as 'Table Name',
